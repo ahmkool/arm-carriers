293664df32745282cbb8f7a914e10daba327d148
ahmkool <ahmed.kulovic@outlook.com>
2026-06-07 20:22:30 +0200

    players drag each other

--- src/weapon/bazooka_specifics.gd
+++ src/weapon/bazooka_specifics.gd
@@ -11,6 +11,7 @@ var _bazooka_bullet_scene: PackedScene = preload("res://src/weapon/bazooka_bulle
 const SHOULDER_HEIGHT := 1.35
 const MIN_CARRIER_DISTANCE := 1.6
 const MAX_CARRIER_DISTANCE := 4.2
+const CARRIER_DRAG_RATIO := 0.2
 const FIRE_COOLDOWN_SEC := 0.75
 const GROUND_COLLISION_MASK := 4
 const GROUND_RAYCAST_UP := 0.5
@@ -23,6 +24,9 @@ var _has_fixed_direction_setter_end := false
 var _fire_cooldown_remaining := 0.0
 var _was_carried := false
 var _previous_carrier_count := 0
+var _prev_shooter_feet := Vector3.ZERO
+var _prev_direction_setter_feet := Vector3.ZERO
+var _has_prev_dual_carry_positions := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -44,6 +48,7 @@ func _physics_process(delta):
 	if not is_carried:
 		_release_weapon_physics()
 		_clear_fixed_endpoints()
+		_clear_dual_carry_position_history()
 		_was_carried = false
 		_previous_carrier_count = 0
 		return
@@ -55,11 +60,12 @@ func _physics_process(delta):
 	var carrier_count := int(has_shooter) + int(has_direction_setter)
 	if carrier_count == 1 and _previous_carrier_count == 2:
 		_anchor_free_end_to_ground(has_shooter, has_direction_setter)
+		_clear_dual_carry_position_history()
 	_previous_carrier_count = carrier_count
 
 	if has_shooter and has_direction_setter:
 		_clear_fixed_endpoints()
-		_enforce_carrier_distance(carry_info.main_carrier, carry_info.secondary_carrier)
+		_enforce_carrier_distance(carry_info.main_carrier, carry_info.secondary_carrier, delta)
 		var shooter_shoulder := _get_player_shoulder_position(carry_info.main_carrier)
 		var direction_setter_shoulder := _get_player_shoulder_position(carry_info.secondary_carrier)
 		_apply_pose_from_ends(shooter_shoulder, direction_setter_shoulder)
@@ -133,6 +139,10 @@ func _apply_pose_from_ends(shooter_end_world: Vector3, direction_setter_end_worl
 func _clear_fixed_endpoints() -> void:
 	_has_fixed_shooter_end = false
 	_has_fixed_direction_setter_end = false
+
+
+func _clear_dual_carry_position_history() -> void:
+	_has_prev_dual_carry_positions = false
 
 
 func _update_solo_shooter_carry(carrier: PlayerLocal) -> void:
@@ -270,18 +280,219 @@ func _update_carrier_collision_exceptions(carry_info: CarryInfo) -> void:
 	_carrier_collision_exceptions = desired_exceptions
 
 
-func _enforce_carrier_distance(shooter: PlayerLocal, direction_setter: PlayerLocal) -> void:
+func _enforce_carrier_distance(
+	shooter: PlayerLocal,
+	direction_setter: PlayerLocal,
+	delta: float
+) -> void:
 	var shooter_pos := shooter.global_position
 	var direction_setter_pos := direction_setter.global_position
-	var carry_delta := direction_setter_pos - shooter_pos
+	var carry_delta := _flat_delta(direction_setter_pos, shooter_pos)
 	var carry_distance := carry_delta.length()
 	if carry_distance < 0.0001:
 		direction_setter.global_position = shooter_pos + Vector3.FORWARD * MIN_CARRIER_DISTANCE
+		_store_dual_carry_positions(shooter, direction_setter)
 		return
 
 	if carry_distance >= MIN_CARRIER_DISTANCE and carry_distance <= MAX_CARRIER_DISTANCE:
+		_store_dual_carry_positions(shooter, direction_setter)
 		return
 
-	var clamped_distance := clampf(carry_distance, MIN_CARRIER_DISTANCE, MAX_CARRIER_DISTANCE)
-	var clamped_direction_setter_pos := shooter_pos + (carry_delta.normalized() * clamped_distance)
-	direction_setter.global_position = clamped_direction_setter_pos
+	var axis := carry_delta / carry_distance
+	if carry_distance > MAX_CARRIER_DISTANCE:
+		_resolve_carrier_distance_violation(
+			shooter,
+			direction_setter,
+			axis,
+			carry_distance,
+			MAX_CARRIER_DISTANCE,
+			true,
+			delta
+		)
+		_store_dual_carry_positions(shooter, direction_setter)
+		return
+
+	_resolve_carrier_distance_violation(
+		shooter,
+		direction_setter,
+		axis,
+		carry_distance,
+		MIN_CARRIER_DISTANCE,
+		false,
+		delta
+	)
+	_store_dual_carry_positions(shooter, direction_setter)
+
+
+func _flat_delta(to_pos: Vector3, from_pos: Vector3) -> Vector3:
+	return Vector3(to_pos.x - from_pos.x, 0.0, to_pos.z - from_pos.z)
+
+
+func _store_dual_carry_positions(shooter: PlayerLocal, direction_setter: PlayerLocal) -> void:
+	_prev_shooter_feet = shooter.global_position
+	_prev_direction_setter_feet = direction_setter.global_position
+	_has_prev_dual_carry_positions = true
+
+
+func _resolve_carrier_distance_violation(
+	shooter: PlayerLocal,
+	direction_setter: PlayerLocal,
+	axis: Vector3,
+	carry_distance: float,
+	target_distance: float,
+	expanding: bool,
+	delta: float
+) -> void:
+	if not _has_prev_dual_carry_positions:
+		_hard_clamp_direction_setter_distance(shooter, direction_setter, axis, target_distance)
+		return
+
+	var leader := _resolve_distance_leader(shooter, direction_setter, axis, expanding)
+	var follower := direction_setter if leader == shooter else shooter
+	var leader_prev := _prev_shooter_feet if leader == shooter else _prev_direction_setter_feet
+	var leader_move := _flat_delta(leader.global_position, leader_prev)
+	var target_follower_pos := _follower_target_feet(
+		leader,
+		follower,
+		shooter,
+		axis,
+		target_distance
+	)
+	var follower_pos := follower.global_position
+	var drag_delta := _flat_delta(target_follower_pos, follower_pos)
+	if drag_delta.length_squared() >= 0.0001:
+		var max_drag := leader_move.length() * CARRIER_DRAG_RATIO
+		if max_drag < 0.0001:
+			max_drag = absf(carry_distance - target_distance) * CARRIER_DRAG_RATIO
+
+		var applied_drag := drag_delta
+		if applied_drag.length() > max_drag:
+			applied_drag = applied_drag.normalized() * max_drag
+
+		follower.global_position = Vector3(
+			follower_pos.x + applied_drag.x,
+			follower_pos.y,
+			follower_pos.z + applied_drag.z
+		)
+		_sync_dragged_velocity(follower, applied_drag, delta)
+
+	var axis_after := _current_carry_axis(shooter, direction_setter)
+	if axis_after.length_squared() < 0.0001:
+		return
+	_clamp_leader_to_tether_distance(
+		leader,
+		follower,
+		shooter,
+		axis_after,
+		target_distance,
+		delta
+	)
+
+
+func _resolve_distance_leader(
+	shooter: PlayerLocal,
+	direction_setter: PlayerLocal,
+	axis: Vector3,
+	expanding: bool
+) -> PlayerLocal:
+	var shooter_delta := _flat_delta(shooter.global_position, _prev_shooter_feet)
+	var setter_delta := _flat_delta(direction_setter.global_position, _prev_direction_setter_feet)
+	if expanding:
+		var shooter_expand := maxf(0.0, -shooter_delta.dot(axis))
+		var setter_expand := maxf(0.0, setter_delta.dot(axis))
+		if setter_expand >= shooter_expand:
+			return direction_setter
+		return shooter
+
+	var shooter_compress := maxf(0.0, shooter_delta.dot(axis))
+	var setter_compress := maxf(0.0, -setter_delta.dot(axis))
+	if setter_compress >= shooter_compress:
+		return direction_setter
+	return shooter
+
+
+func _follower_target_feet(
+	leader: PlayerLocal,
+	follower: PlayerLocal,
+	shooter: PlayerLocal,
+	axis: Vector3,
+	target_distance: float
+) -> Vector3:
+	var leader_pos := leader.global_position
+	var distance_sign := 1.0 if leader == shooter else -1.0
+	return Vector3(
+		leader_pos.x + axis.x * target_distance * distance_sign,
+		follower.global_position.y,
+		leader_pos.z + axis.z * target_distance * distance_sign
+	)
+
+
+func _current_carry_axis(shooter: PlayerLocal, direction_setter: PlayerLocal) -> Vector3:
+	var carry_delta := _flat_delta(direction_setter.global_position, shooter.global_position)
+	if carry_delta.length_squared() < 0.0001:
+		return Vector3.ZERO
+	return carry_delta.normalized()
+
+
+func _leader_target_feet(
+	leader: PlayerLocal,
+	follower: PlayerLocal,
+	shooter: PlayerLocal,
+	axis: Vector3,
+	target_distance: float
+) -> Vector3:
+	var follower_pos := follower.global_position
+	if leader == shooter:
+		return Vector3(
+			follower_pos.x - axis.x * target_distance,
+			leader.global_position.y,
+			follower_pos.z - axis.z * target_distance
+		)
+	return Vector3(
+		follower_pos.x + axis.x * target_distance,
+		leader.global_position.y,
+		follower_pos.z + axis.z * target_distance
+	)
+
+
+func _clamp_leader_to_tether_distance(
+	leader: PlayerLocal,
+	follower: PlayerLocal,
+	shooter: PlayerLocal,
+	axis: Vector3,
+	target_distance: float,
+	delta: float
+) -> void:
+	var leader_pos := leader.global_position
+	var target_leader := _leader_target_feet(leader, follower, shooter, axis, target_distance)
+	var clamp_delta := _flat_delta(target_leader, leader_pos)
+	if clamp_delta.length_squared() < 0.0001:
+		return
+	leader.global_position = Vector3(
+		target_leader.x,
+		leader_pos.y,
+		target_leader.z
+	)
+	_sync_dragged_velocity(leader, clamp_delta, delta)
+
+
+func _hard_clamp_direction_setter_distance(
+	shooter: PlayerLocal,
+	direction_setter: PlayerLocal,
+	axis: Vector3,
+	target_distance: float
+) -> void:
+	var shooter_pos := shooter.global_position
+	direction_setter.global_position = Vector3(
+		shooter_pos.x + axis.x * target_distance,
+		direction_setter.global_position.y,
+		shooter_pos.z + axis.z * target_distance
+	)
+
+
+func _sync_dragged_velocity(player: PlayerLocal, flat_drag: Vector3, delta: float) -> void:
+	if delta <= 0.0001:
+		return
+	var drag_velocity := flat_drag / delta
+	player.velocity.x = drag_velocity.x
+	player.velocity.z = drag_velocity.z
